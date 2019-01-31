c3b00c8bb56ba6878dd05a5739104c632f362954
Ryan Dockstader <ryanldockstader@gmail.com>
2019-01-31 00:15:04 -0700

    add movie database link to AJAX page

--- CIT261/Web/03-AJAX/script.js
+++ CIT261/Web/03-AJAX/script.js
@@ -52,6 +52,52 @@ var getRandomPokemon = function() {
 
 }
 
+
+var searchMovie = function() {
+    var title = document.getElementById('movie-title').value;
+    var year = document.getElementById('movie-year').value;
+    var type = document.getElementById('movie-type').value;
+    
+    var url = 'http://www.omdbapi.com/?apikey=980b75d0';
+    if(title != null && title != '') {
+        url += '&s=' + title;
+    } else {
+        // throw error here
+        return;
+    }
+    if(year != null && year != '' && !isNaN(year)) {
+        url += '&y=' + year;
+    }
+    if(type != null && type != '') {
+        url += '&type=' + type;
+    }
+    
+    getJSON(url, null, null, function(resp) {
+        var result = JSON.parse(resp);
+        var html = '';
+        if(result.Response != 'False') {
+            result.Search.forEach(function(ele) {
+            html += '<h3>' + ele.Title + '</h3>';
+            html += '<h4>' + ele.Year + ' ~ ' + ele.Type + '</h4>';
+            if(ele.Poster != 'N/A') {
+                html += '<img src="' + ele.Poster + '" alt="' + ele.Title + '">';
+            } else {
+                html += '<p><em> No poster available </em></p>';
+            }
+            
+        })
+        
+        html += '<p><em> Total Results: ' + result.totalResults + '</em></p>';
+        } else {
+            html += '<p> No Results Found </p>';
+        }
+        
+        document.getElementById('movie-display').innerHTML = html;
+    })
+    
+    
+}
+
 getJSON('./pokemon.json', null, null, function(resp) {
     kanto = JSON.parse(resp)['regions'][0];
 });
